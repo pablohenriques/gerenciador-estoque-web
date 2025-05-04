0669452f3702542d88702239778a7054d5cde5c4
Pablo <ph.info.cont@rede.ulbra.br>
2025-05-04 19:09:44 -0300

    Refatoracao projeto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
-import Header from "./components/header/Header.jsx";
-import Footer from "./components/footer/Footer.jsx";
-import Section from "./components/section/Section.jsx";
+import Header from "./components/layout/header/Header.jsx";
+import Footer from "./components/layout/footer/Footer.jsx";
+import Section from "./components/layout/section/Section.jsx";
 
 function App() {
 
--- src/components/layout/main/Main.jsx
+++ src/components/layout/main/Main.jsx
@@ -1,10 +1,12 @@
 import './Main.css';
+import DepartamentoList from "../../features/DepartamentoList/DepartamentoList.jsx";
 
 export default function Main() {
     return(
         <main>
             <h1>Conteúdo da Página</h1>
             <p>Este é o conteúdo da página</p>
+            <DepartamentoList/>
         </main>
     );
 }
